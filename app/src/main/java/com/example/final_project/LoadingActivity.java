9ee98694bf33447110d31b76babef1cd5e905a05
Bach Phung <quangbach19112000@gmail.com>
2022-04-29 16:00:16 +0300

    Upgrade Rendering Performance

--- app/src/main/java/com/example/final_project/LoadingActivity.java
+++ app/src/main/java/com/example/final_project/LoadingActivity.java
@@ -1,21 +1,135 @@
 package com.example.final_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
+import android.widget.Toast;
 
-public class LoadingActivity extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.JsonParser;
 
+public class LoadingActivity extends AppCompatActivity implements LocationListener {
+
+  //variables
+  private RequestQueue queue;
+  private double longitude = 0.0;
+  private double latitude = 0.0;
+  private boolean firstRender = true;
+  String weatherResponse="", cityNameResponse="";
+  StringRequest weatherRequest, cityNameRequest;
+  LocationManager locationManager;
+
+  //Methods
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_loading);
-    Handler handler = new Handler();
-    handler.postDelayed(() -> {
-      startActivity(new Intent(LoadingActivity.this, MainActivity.class));
+    queue = Volley.newRequestQueue(this);
+    startGPS();
+    new PrefetchData().execute();
+  }
+  @Override
+  protected void onPause(){
+    super.onPause();
+    locationManager.removeUpdates(this); // Unregister location listener
+  }
+  private String urlWeather() {
+    return String.format("https://api.openweathermap.org/data/2.5/onecall?lat=%.4f&lon=%.4f&exclude=minutely&appid=d8288cede8b8b1149b236932c004dc6a", this.latitude, this.longitude);
+  }
+  private String urlCityName() {
+    return String.format("https://api.openweathermap.org/geo/1.0/reverse?lat=%.4f&lon=%.4f&limit=1&appid=d8288cede8b8b1149b236932c004dc6a", this.latitude, this.longitude) ;
+  }
+
+  private void initialDataLoading(){
+    this.weatherRequest = new StringRequest(Request.Method.GET, urlWeather(), response -> {
+      try{
+        this.weatherResponse = response;
+      }
+      catch (Exception e){}
+    }, error -> {
+      Toast.makeText(this, error.toString(), Toast.LENGTH_LONG).show();
+    });
+    this.cityNameRequest = new StringRequest(Request.Method.GET, urlCityName(),response -> {
+      try{
+        this.cityNameResponse = response;
+      }
+      catch (Exception e){}
+    }, error -> {
+      Toast.makeText(this, error.toString(), Toast.LENGTH_LONG).show();
+    });
+
+  }
+  public void startGPS() {
+    //Todo: Start listening to user's location through Location Manager
+    //Todo: - location permission specified in android manifest file
+    //Todo: - ask user's permission run-time before accessing GPS
+    locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+    try {
+      if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        // TODO: Check if the user has granted permission for GPS
+        // Todo: if not, let's prompt and ask the user to give it
+        ActivityCompat.requestPermissions(this,
+          new String[] {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},0
+        );
+        return;
+      }
+      locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+    }
+    catch (Exception e){
+      Log.d("Error", e.toString());
+    }
+  }
+  @Override
+  public void onLocationChanged(@NonNull Location location) {
+    this.longitude = location.getLongitude();
+    this.latitude = location.getLatitude();
+    Log.d("Lat Splash: ", String.valueOf(this.latitude));
+    Log.d("Long Splash: ", String.valueOf(this.longitude));
+    initialDataLoading();
+    queue.add(weatherRequest);
+    queue.add(cityNameRequest);
+  }
+
+  private class PrefetchData extends AsyncTask<Void, Void, Void> {
+
+    @Override
+    protected synchronized Void doInBackground(Void... arg0) {
+      Log.d("DO IN BACKGROUND", "called");
+      // Wait for fetch weather Request complete
+      while (weatherResponse.length() == 0){}
+      // Wait for fetch city name Request complete
+      while (cityNameResponse.length() == 0){}
+      return null;
+    }
+    @Override
+    protected synchronized void onPostExecute(Void result){
+      Log.d("ON POST EXECUTE", "called");
+      super.onPostExecute(result);
+      Log.d("WeatherRes", weatherResponse);
+      Log.d("CityNameRes", cityNameResponse);
+      Intent i = new Intent(LoadingActivity.this, MainActivity.class);
+      //Todo: Send data to main activity
+      i.putExtra("Weather", weatherResponse);
+      i.putExtra("CityName", cityNameResponse);
+      i.putExtra("Long",  String.valueOf(longitude));
+      i.putExtra("Lat", String.valueOf(latitude));
+      startActivity(i);
+      //Todo: Finish Loading Activity
       finish();
-    },3000);
+    }
   }
 }
--- app/src/main/java/com/example/final_project/MainActivity.java
+++ app/src/main/java/com/example/final_project/MainActivity.java
@@ -7,22 +7,16 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.Manifest;
-import android.app.Activity;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
@@ -42,13 +36,15 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
   private ArrayList<String> hours = new ArrayList<>();
   private ArrayList<String> iconList = new ArrayList<>();
   private ArrayList<String> degList = new ArrayList<>();
-
   //daily vars
   private ArrayList<String> dateList = new ArrayList<>();
   private ArrayList<String> iconDailyList = new ArrayList<>();
   private ArrayList<String> highestList = new ArrayList<>();
   private ArrayList<String> lowestList = new ArrayList<>();
-
+  private String intentWeatherResponse;
+  private String intentCityName;
+  private String intentLong;
+  private String intentLat;
   private RequestQueue queue;
   private double longitude = 0.0;
   private double latitude = 0.0;
@@ -71,85 +67,49 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     setContentView(R.layout.activity_main);
     queue = Volley.newRequestQueue(this);
     startGPS();
+    intentWeatherResponse = getIntent().getStringExtra("Weather");
+    intentCityName = getIntent().getStringExtra("CityName");
+    intentLong = getIntent().getStringExtra("Long");
+    intentLat = getIntent().getStringExtra("Lat");
+    try {
+      parseWeatherData(intentWeatherResponse);
+      parseCityNameData(intentCityName);
+    } catch (JSONException e) {
+      e.printStackTrace();
+    }
+
   }
+
 
   private void initialData() {
     //  1. Make a request to weather map api
     StringRequest stringRequest = new StringRequest(Request.Method.GET, urlWeather(),
       response -> {
-        // This is callback for successful response
-        // Parse response into JSON Object for processing
-        try {
-          JSONObject fetchedData = new JSONObject(response);
-          curWeather = gson.fromJson(fetchedData.getJSONObject("current").toString(), CurrentWeather.class);
-          dailyWeathers = gson.fromJson(fetchedData.getJSONArray("daily").toString(), DailyWeather[].class);
-          hourlyWeathers = gson.fromJson(fetchedData.getJSONArray("hourly").toString(), HourlyWeather[].class);
-          hours.clear();
-          iconList.clear();
-          degList.clear();
-          for (int i = 0; i < 24; i++) {
-            if (i == 0) {
-              this.hours.add("Now");
-            } else {
-              Date tempDate = parseUnixToDate(hourlyWeathers[i].getDt());
-              String tempHour = getHour(tempDate);
-              this.hours.add(tempHour);
-            }
-            this.iconList.add(String.format("https://openweathermap.org/img/wn/%s@2x.png", (hourlyWeathers[i].getWeather().get(0).getIcon())));
-            this.degList.add(String.format("%d\u00B0", Math.round(hourlyWeathers[i].getTemp() - 273.15)));
-          }
-          dateList.clear();
-          iconDailyList.clear();
-          highestList.clear();
-          lowestList.clear();
-          for (int j = 0; j < 7; j++) {
-            Date date = parseUnixToDate(dailyWeathers[j + 1].getDt());
-            String weekday = getDate(date);
-            this.dateList.add(weekday);
-            Log.d("Day: ", dateList.get(j));
-
-            this.iconDailyList.add(String.format("https://openweathermap.org/img/wn/%s@2x.png", dailyWeathers[j + 1].getWeather().get(0).getIcon()));
-            this.highestList.add(String.valueOf(Math.round(dailyWeathers[j + 1].getTempMax() - 273.15)));
-            this.lowestList.add(String.valueOf(Math.round(dailyWeathers[j + 1].getTempMin() - 273.15)));
-          }
-          initRecyclerView();
-          TextView curWeatherDesc = findViewById(R.id.weatherDesc);
-          TextView curWeatherDeg = findViewById(R.id.weatherDeg);
-          TextView curWeatherHi_Li = findViewById(R.id.high_low);
-
-          curWeatherDeg.setText(String.format("%d\u00B0", Math.round(curWeather.getTemp() - 273.15)));
-          curWeatherDesc.setText(curWeather.getWeather().get(0).getMain());
-          curWeatherHi_Li.setText(String.format("H:%d\u00B0 L:%d\u00B0",Math.round(dailyWeathers[0].getTempMax() - 273.15), Math.round(dailyWeathers[0].getTempMin() - 273.15) ));
-        } catch (JSONException e) {
-        }
+        try { parseWeatherData(response);}
+        catch (JSONException e) {e.printStackTrace();}
       },
       error -> {
-        // This is callback for whenever something went wrong with fetch
-        Toast.makeText(this, error.toString(), Toast.LENGTH_LONG).show();
+        error.printStackTrace();
       }
     );
     StringRequest getCityNameRequest = new StringRequest(Request.Method.GET, urlCityName(),
       response -> {
         try{
-          JSONArray fetchedDataArr = new JSONArray(response);
-          JSONObject fetchedData = fetchedDataArr.getJSONObject(0);
-          String cityName =  fetchedData.getString("name");
-          TextView cityNameTextView = findViewById(R.id.cityName);
-          cityNameTextView.setText(cityName);
+          parseCityNameData(response);
         }
-        catch (Exception e){
-          Log.d ("City Name: ", e.toString());
-        }
+        catch (JSONException e) {e.printStackTrace();}
       },
       error -> {
-
+        error.printStackTrace();
       }
       );
-    //  2. Send the request by adding it to the queue
 
+    //  2. Send the request by adding it to the queue
     queue.add(stringRequest);
     queue.add(getCityNameRequest);
   }
+
+
 
   private void initRecyclerView() {
     // Hourly
@@ -163,7 +123,54 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     RecyclerViewDailyAdapter adapter2 = new RecyclerViewDailyAdapter(this, dateList, iconDailyList, highestList, lowestList);
     recyclerViewDaily.setAdapter(adapter2);
   }
+  private void parseWeatherData(String response) throws JSONException {
+    JSONObject fetchedWeatherData = new JSONObject(response);
+    curWeather = gson.fromJson(fetchedWeatherData.getJSONObject("current").toString(), CurrentWeather.class);
+    dailyWeathers = gson.fromJson(fetchedWeatherData.getJSONArray("daily").toString(), DailyWeather[].class);
+    hourlyWeathers = gson.fromJson(fetchedWeatherData.getJSONArray("hourly").toString(), HourlyWeather[].class);
+    hours.clear();
+    iconList.clear();
+    degList.clear();
+    dateList.clear();
+    iconDailyList.clear();
+    highestList.clear();
+    lowestList.clear();
+    for (int i = 0; i < 24; i++) {
+      if (i == 0) {
+        this.hours.add("Now");
+      }
+      else {
+          this.hours.add(getHour(parseUnixToDate(hourlyWeathers[i].getDt())));
+      }
+      if(i <7){
+        Date date = parseUnixToDate(dailyWeathers[i + 1].getDt());
+        String weekday = getDate(date);
+        this.dateList.add(weekday);
+        Log.d("Day: ", dateList.get(i));
 
+        this.iconDailyList.add(String.format("https://openweathermap.org/img/wn/%s@2x.png", dailyWeathers[i + 1].getWeather().get(0).getIcon()));
+        this.highestList.add(String.valueOf(Math.round(dailyWeathers[i + 1].getTempMax() - 273.15)));
+        this.lowestList.add(String.valueOf(Math.round(dailyWeathers[i + 1].getTempMin() - 273.15)));
+      }
+      this.iconList.add(String.format("https://openweathermap.org/img/wn/%s@2x.png", (hourlyWeathers[i].getWeather().get(0).getIcon())));
+      this.degList.add(String.format("%d\u00B0", Math.round(hourlyWeathers[i].getTemp() - 273.15)));
+    }
+    initRecyclerView();
+    TextView curWeatherDesc = findViewById(R.id.weatherDesc);
+    TextView curWeatherDeg = findViewById(R.id.weatherDeg);
+    TextView curWeatherHi_Li = findViewById(R.id.high_low);
+
+    curWeatherDeg.setText(String.format("%d\u00B0", Math.round(curWeather.getTemp() - 273.15)));
+    curWeatherDesc.setText(curWeather.getWeather().get(0).getMain());
+    curWeatherHi_Li.setText(String.format("H:%d\u00B0 L:%d\u00B0",Math.round(dailyWeathers[0].getTempMax() - 273.15), Math.round(dailyWeathers[0].getTempMin() - 273.15) ));
+  }
+  private void parseCityNameData (String response) throws JSONException{
+    JSONArray fetchedDataArr = new JSONArray(response);
+    JSONObject fetchedData = fetchedDataArr.getJSONObject(0);
+    String cityName =  fetchedData.getString("name");
+    TextView cityNameTextView = findViewById(R.id.cityName);
+    cityNameTextView.setText(cityName);
+  }
   private Date parseUnixToDate(long dateUnix) {
     return Date.from(Instant.ofEpochSecond(dateUnix));
   }
@@ -209,6 +216,22 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     this.latitude = location.getLatitude();
     Log.d("Long: ", String.valueOf(this.longitude) );
     Log.d("Lat: ", String.valueOf(this.latitude));
-    initialData();
+    if(String.valueOf(this.latitude).equals(this.intentLat)  || String.valueOf(this.longitude).equals(this.intentLong) ){
+      Log.d ("DONT_FETCH", "called");
+      Log.d ("Long: ", intentLong);
+      Log.d ("Lat: ", intentLat);
+      try {
+        parseWeatherData(intentWeatherResponse);
+        parseCityNameData(intentCityName);
+      } catch (JSONException e) {
+        e.printStackTrace();
+      }
+    }
+    else{
+      Log.d ("RE_FETCH", "called");
+      Log.d ("INTEBT_Long: ", intentLong);
+      Log.d ("INTENT_Lat: ", intentLat);
+      initialData();
+    }
   }
 }
